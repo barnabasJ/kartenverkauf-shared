081b3600b1dc009ae782cf5e82534188313aaeb8
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2019-10-20 15:03:29 +0200

    fix: add needed Exception to interface method

--- src/main/java/at/fhv/teama/kartenverkauf/rmi/EasyTicketService.java
+++ src/main/java/at/fhv/teama/kartenverkauf/rmi/EasyTicketService.java
@@ -2,7 +2,8 @@ package at.fhv.teama.kartenverkauf.rmi;
 
 import at.fhv.teama.kartenverkauf.dto.AddressDto;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface EasyTicketService extends Remote {
-    void saveAddress(AddressDto addressDto);
+    void saveAddress(AddressDto addressDto) throws RemoteException;
 }
