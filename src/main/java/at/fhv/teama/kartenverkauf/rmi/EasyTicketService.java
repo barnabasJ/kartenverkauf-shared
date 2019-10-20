17490dee0cefcd0e543b46d91ee45a56ef1ecc0b
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2019-10-20 14:28:59 +0200

    fix: add marker Interface

--- src/main/java/at/fhv/teama/kartenverkauf/rmi/EasyTicketService.java
+++ src/main/java/at/fhv/teama/kartenverkauf/rmi/EasyTicketService.java
@@ -1,7 +1,8 @@
 package at.fhv.teama.kartenverkauf.rmi;
 
 import at.fhv.teama.kartenverkauf.dto.AddressDto;
+import java.rmi.Remote;
 
-public interface EasyTicketService {
+public interface EasyTicketService extends Remote {
     void saveAddress(AddressDto addressDto);
 }
