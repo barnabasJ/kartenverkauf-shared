7ad41619a0c806d261f3dba1b587ea530cec625d
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2019-10-20 14:48:29 +0200

    fix: add marker interface to dto

--- src/main/java/at/fhv/teama/kartenverkauf/dto/AddressDto.java
+++ src/main/java/at/fhv/teama/kartenverkauf/dto/AddressDto.java
@@ -2,6 +2,8 @@ package at.fhv.teama.kartenverkauf.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class AddressDto {
+public class AddressDto implements Serializable {
 }
